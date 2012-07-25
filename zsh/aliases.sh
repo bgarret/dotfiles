44ae4a94bbf148d30e8c3eebd2b91201824c1d09
Benoit Garret <benoit.garret@gadz.org>
2012-07-25 10:59:22 +0200

    Add sudo, ls and grep aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -10,4 +10,8 @@ alias gs='git stash'
 alias gsp='git stash pop'
 alias nano="vim"
 alias restartapp="touch tmp/restart.txt"
-alias grep="grep --color"
+alias grep="grep --color=auto"
+alias ls="ls --color=auto"
+
+alias sudo="sudo -E"
+alias -- +="sudo"
