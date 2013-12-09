d1c18bcf8f4ede8dd6cbfc75ff116f94a23490e6
Benoit Garret <benoit.garret@gadz.org>
2013-12-09 10:52:26 +0100

    Remove the stash aliases, they conflict with ghostscript

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,8 +6,8 @@ compdef _git gl=git-pull
 alias gp='git push'
 compdef _git gp=git-push
 alias gg='gitg &'
-alias gs='git stash'
-alias gsp='git stash pop'
+#alias gs='git stash'
+#alias gsp='git stash pop'
 alias nano="vim"
 alias restartapp="touch tmp/restart.txt"
 alias grep="grep --color=auto"
